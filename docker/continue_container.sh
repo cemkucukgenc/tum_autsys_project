ccf2f31455a06cfe8b9e22925ea6f7cb53a40336
Cem Küçükgenç <cem.kucukgenc@tum.de>
2024-02-11 23:33:11 +0100

    new melodic version

--- docker/continue_container.sh
+++ docker/continue_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-container_name="autsys_container" # Change this to your container name
+container_name="autsys_container_melodic" # Change this to your container name
 catkin_ws_path="/autsys_ws/tum_autsys_project/catkin_ws" # Change this to your catkin_ws path
 
 # ANSI escape codes for colors
@@ -10,15 +10,15 @@ YELLOW='\033[0;33m'
 RESET='\033[0m'
 
 sudo docker exec -it $container_name bash -c "
-    if [ -f '/opt/ros/noetic/setup.bash' ]; then
-        source /opt/ros/noetic/setup.bash && \
+    if [ -f '/opt/ros/melodic/setup.bash' ]; then
+        source /opt/ros/melodic/setup.bash && \
         if [ \$? -eq 0 ]; then
             echo -e '${GREEN}ROS distro sourced as${RESET}' \$ROS_DISTRO 
         else
             echo -e '${RED}Failed to source ROS distro${RESET}' 
         fi
     else
-        echo -e '${YELLOW}/opt/ros/noetic/setup.bash does not exist yet. Skipping sourcing.${RESET}'
+        echo -e '${YELLOW}/opt/ros/melodic/setup.bash does not exist yet. Skipping sourcing.${RESET}'
     fi && \
     if [ -f '$catkin_ws_path/devel/setup.bash' ]; then
         source $catkin_ws_path/devel/setup.bash && \
