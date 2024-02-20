1f1b467b60c216f81ed070b407173e505943edc8
Serdar Soyer <serdar.soyer@tum.de>
2024-02-20 20:28:13 +0300

    light_detector node initialized

---
+++ catkin_ws/src/vision_pkg/src/light_detector.cpp
@@ -0,0 +1,30 @@
+#include <cmath>
+#include <cv_bridge/cv_bridge.h>
+#include <depth_image_proc/depth_traits.h>
+#include <eigen3/Eigen/Dense>
+#include <eigen_conversions/eigen_msg.h>
+#include <image_geometry/pinhole_camera_model.h>
+#include <image_transport/image_transport.h>
+#include <limits>
+#include <math.h>
+#include <nav_msgs/Odometry.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl_ros/transforms.h>
+#include <ros/ros.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/point_cloud2_iterator.h>
+#include <tf/transform_datatypes.h>
+#include <tf/transform_listener.h>
+#include <visualization_msgs/Marker.h>
+
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "object_detector_node");
+  ObjectDetectorNode node;
+  ros::spin();
+}
