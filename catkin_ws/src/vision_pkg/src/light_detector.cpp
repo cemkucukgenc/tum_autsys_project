cf46569042c55ca3ab737434364b7a9865640c6d
Serdar Soyer <serdar.soyer@kuartis.com>
2024-02-25 21:43:35 +0300

    depth image finalized

--- catkin_ws/src/vision_pkg/src/light_detector.cpp
+++ catkin_ws/src/vision_pkg/src/light_detector.cpp
@@ -1,20 +1,20 @@
-#include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
+#include <image_geometry/pinhole_camera_model.h>
+#include <image_transport/image_transport.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <ros/ros.h>
 #include <tf/transform_datatypes.h>
 #include <tf/transform_listener.h>
-#include <ros/ros.h>
-#include <image_geometry/pinhole_camera_model.h>
 
+#include <depth_image_proc/depth_traits.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl_ros/transforms.h>
-#include <depth_image_proc/depth_traits.h>
 #include <sensor_msgs/Image.h>
-#include <sensor_msgs/point_cloud2_iterator.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/point_cloud2_iterator.h>
 
 class LightDetectorNode {
   ros::NodeHandle nh_;
@@ -51,30 +51,31 @@ public:
     createPointCloudFromDepthImage(cloud_msg, masked_depth_image);
 
     auto world_frame_cloud = transformPointCloudToGlobalFrame(cloud_msg);
-    if (world_frame_cloud.points.empty()) 
-    {
-    ROS_ERROR("Point cloud is empty!");
-    return;
+    if (world_frame_cloud.points.empty()) {
+      ROS_ERROR("Point cloud is empty!");
+      return;
     }
     auto detection_center = calculatePointCloudCenter(world_frame_cloud);
 
     if (checkForNewDetection(detection_center)) {
-      ROS_WARN("Light detected at %f, %f, %f", detection_center.x, detection_center.y, detection_center.z);
+      ROS_WARN("Light detected at %f, %f, %f", detection_center.x,
+               detection_center.y, detection_center.z);
       detections_.push_back(detection_center);
     }
   }
 
   void onDepthImageReceived(const sensor_msgs::ImageConstPtr &depth_image_msg) {
-    // TODO
+    last_depth_image_ = *depth_image_msg;
   }
 
   void onDepthInfoReceived(const sensor_msgs::CameraInfo &depth_info_msg) {
-    // TODO
+    last_depth_info_ = depth_info_msg;
+    camera_model_.fromCameraInfo(depth_info_msg);
   }
 
 private:
-
-  void createPointCloudFromDepthImage(sensor_msgs::PointCloud2::Ptr& cloud_msg, const cv::Mat& depth_image) {
+  void createPointCloudFromDepthImage(sensor_msgs::PointCloud2::Ptr &cloud_msg,
+                                      const cv::Mat &depth_image) {
     cloud_msg->header = last_depth_image_.header;
     cloud_msg->height = last_depth_image_.height;
     cloud_msg->width = last_depth_image_.width;
@@ -87,28 +88,33 @@ private:
     depthImageToPointCloud(depth_image, cloud_msg, camera_model_);
   }
 
-  pcl::PointCloud<pcl::PointXYZ> transformPointCloudToGlobalFrame(sensor_msgs::PointCloud2::Ptr cloud_msg) {
+  pcl::PointCloud<pcl::PointXYZ>
+  transformPointCloudToGlobalFrame(sensor_msgs::PointCloud2::Ptr cloud_msg) {
     pcl::PointCloud<pcl::PointXYZ> local_cloud;
     pcl::fromROSMsg(*cloud_msg, local_cloud);
 
     tf::StampedTransform transform;
     try {
-      transform_listener_.waitForTransform("world", cloud_msg->header.frame_id, ros::Time(0), ros::Duration(10.0));
-      transform_listener_.lookupTransform("world", cloud_msg->header.frame_id, ros::Time(0), transform);
-    } catch (tf::TransformException& ex) {
+      transform_listener_.waitForTransform("world", cloud_msg->header.frame_id,
+                                           ros::Time(0), ros::Duration(10.0));
+      transform_listener_.lookupTransform("world", cloud_msg->header.frame_id,
+                                          ros::Time(0), transform);
+    } catch (tf::TransformException &ex) {
       ROS_ERROR("%s", ex.what());
     }
 
     pcl::PointCloud<pcl::PointXYZ> global_cloud;
     pcl_ros::transformPointCloud(local_cloud, global_cloud, transform);
     return global_cloud;
-    }
-  
-  pcl::PointXYZ calculatePointCloudCenter(const pcl::PointCloud<pcl::PointXYZ>& cloud) {
+  }
+
+  pcl::PointXYZ
+  calculatePointCloudCenter(const pcl::PointCloud<pcl::PointXYZ> &cloud) {
     pcl::PointXYZ center;
     int valid_points = 0;
-    for (const auto& point : cloud.points) {
-      if (!std::isnan(point.x) && !std::isnan(point.y) && !std::isnan(point.z)) {
+    for (const auto &point : cloud.points) {
+      if (!std::isnan(point.x) && !std::isnan(point.y) &&
+          !std::isnan(point.z)) {
         center.x += point.x;
         center.y += point.y;
         center.z += point.z;
@@ -123,15 +129,16 @@ private:
     return center;
   }
 
-  void depthImageToPointCloud(
-    const cv::Mat& depth_image,
-    sensor_msgs::PointCloud2::Ptr& cloud_msg,
-    const image_geometry::PinholeCameraModel& camera_model,
-    double range_max = 0.0) {
+  void
+  depthImageToPointCloud(const cv::Mat &depth_image,
+                         sensor_msgs::PointCloud2::Ptr &cloud_msg,
+                         const image_geometry::PinholeCameraModel &camera_model,
+                         double range_max = 0.0) {
     float center_x = camera_model.cx();
     float center_y = camera_model.cy();
 
-    double unit_scaling = depth_image_proc::DepthTraits<uint16_t>::toMeters(uint16_t(1));
+    double unit_scaling =
+        depth_image_proc::DepthTraits<uint16_t>::toMeters(uint16_t(1));
     float constant_x = unit_scaling / camera_model.fx();
     float constant_y = unit_scaling / camera_model.fy();
     float bad_point = std::numeric_limits<float>::quiet_NaN();
@@ -140,16 +147,19 @@ private:
     sensor_msgs::PointCloud2Iterator<float> iter_y(*cloud_msg, "y");
     sensor_msgs::PointCloud2Iterator<float> iter_z(*cloud_msg, "z");
 
-    const uint16_t* depth_row = reinterpret_cast<const uint16_t*>(depth_image.data);
+    const uint16_t *depth_row =
+        reinterpret_cast<const uint16_t *>(depth_image.data);
     int row_step = depth_image.step / sizeof(uint16_t);
 
     for (int v = 0; v < (int)cloud_msg->height; ++v, depth_row += row_step) {
-      for (int u = 0; u < (int)cloud_msg->width; ++u, ++iter_x, ++iter_y, ++iter_z) {
+      for (int u = 0; u < (int)cloud_msg->width;
+           ++u, ++iter_x, ++iter_y, ++iter_z) {
         uint16_t depth = depth_row[u];
 
         if (!depth_image_proc::DepthTraits<uint16_t>::valid(depth)) {
           if (range_max != 0.0) {
-            depth = depth_image_proc::DepthTraits<uint16_t>::fromMeters(range_max);
+            depth =
+                depth_image_proc::DepthTraits<uint16_t>::fromMeters(range_max);
           } else {
             *iter_x = *iter_y = *iter_z = bad_point;
             continue;
@@ -178,9 +188,11 @@ private:
     return depth_image->image;
   }
 
-  bool checkForNewDetection(const pcl::PointXYZ& point) {
-    for (const auto& detection : detections_) {
-      double distance_squared = std::pow(detection.x - point.x, 2) + std::pow(detection.y - point.y, 2) + std::pow(detection.z - point.z, 2);
+  bool checkForNewDetection(const pcl::PointXYZ &point) {
+    for (const auto &detection : detections_) {
+      double distance_squared = std::pow(detection.x - point.x, 2) +
+                                std::pow(detection.y - point.y, 2) +
+                                std::pow(detection.z - point.z, 2);
       if (distance_squared < 100) {
         return false;
       }
