39da062a9be3638d8da1cfe1098bec2bd491c273
Erencan Aslakci <ge97jed@mytum.de>
2024-02-25 18:23:09 +0300

    c make and package editted

--- catkin_ws/src/vision_pkg/src/light_detector.cpp
+++ catkin_ws/src/vision_pkg/src/light_detector.cpp
@@ -1,26 +1,17 @@
-#include <cmath>
-#include <cv_bridge/cv_bridge.h>
-#include <depth_image_proc/depth_traits.h>
-#include <eigen3/Eigen/Dense>
-#include <eigen_conversions/eigen_msg.h>
-#include <image_geometry/pinhole_camera_model.h>
 #include <image_transport/image_transport.h>
-#include <limits>
-#include <math.h>
-#include <nav_msgs/Odometry.h>
+#include <cv_bridge/cv_bridge.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <tf/transform_datatypes.h>
+#include <tf/transform_listener.h>
+#include <ros/ros.h>
+#include <image_geometry/pinhole_camera_model.h>
+
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl_ros/transforms.h>
-#include <ros/ros.h>
-#include <sensor_msgs/Image.h>
-#include <sensor_msgs/PointCloud2.h>
-#include <sensor_msgs/point_cloud2_iterator.h>
-#include <tf/transform_datatypes.h>
-#include <tf/transform_listener.h>
-#include <visualization_msgs/Marker.h>
+
 
 class LightDetectorNode {
   ros::NodeHandle nh_;
